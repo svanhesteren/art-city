6fbb6c38e1b17fa7cd189d5fb5b788dd6bcd1c15
svanhesteren <s.vanhesteren@gmail.com>
2017-12-09 16:40:01 +0100

    Add new edit (repair)

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -7,4 +7,38 @@ class ArtworksController < ApplicationController
     @artwork = Artwork.find(params[:id])
   end
 
+  def new
+    @artwork = Artwork.new
+  end
+
+  def edit
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+
+    if @artwork.save
+      redirect_to @artwork
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @artwork = Artwork.find(params[:id])
+
+    if @artwork.update_attributes(artwork_params)
+      redirect_to @artwork
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:kind, :name, :artist, :price, :image_url, :museum_id)
+  end
+
 end
